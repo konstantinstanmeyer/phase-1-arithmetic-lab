712778c19a3c5901bc0cded7fd15f645f791018b
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-08-08 16:56:10 -0600

    commit tree 6785b262a09b711e05a26616620ee7fbb47cb2fd

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 24;
+const num4 = 5;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = (Math.floor(Math.random() * 1000) + 1);
+
+const mod = (num3 % num4);
+
+const max = Math.max(1, 2, 3, 4, 20);
